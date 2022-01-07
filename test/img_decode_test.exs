87159d2bb21dc0fe2994ff10a75350163b966992
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2022-01-07 16:10:25 +0000

    use multiple color in test images
    ensure the accessing order for rows and cols is expected
    
    Signed-off-by: Cocoa <89497197+cocoa-xu@users.noreply.github.com>

--- test/img_decode_test.exs
+++ test/img_decode_test.exs
@@ -5,26 +5,26 @@ defmodule ImgDecodeTest do
   test "decode png from file" do
     {:ok, img, shape, type} = ImgDecode.from_file(Path.join(__DIR__, "test.png"))
     assert type == :u8
-    assert shape == {2, 3, 3}
-    assert img == <<248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248,
-             248, 248, 248>>
+    assert shape == {2, 3, 4}
+    assert img == <<241, 145, 126, 255, 136, 190, 78, 255, 68, 122, 183, 255, 244, 196, 187, 255,
+             190, 205, 145, 255, 144, 184, 200, 255>>
   end
 
   test "decode jpg from file" do
     {:ok, img, shape, type} = ImgDecode.from_file(Path.join(__DIR__, "test.jpg"))
     assert type == :u8
     assert shape == {2, 3, 3}
-    assert img == <<248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248,
-             248, 248, 248>>
+    assert img == <<180, 128, 70, 148, 128, 78, 89, 134, 101, 222, 170, 112, 182, 162, 112, 112,
+             157, 124>>
   end
 
   test "decode png from memory" do
     {:ok, buffer} = File.read(Path.join(__DIR__, "test.png"))
     {:ok, img, shape, type} = ImgDecode.from_memory(buffer)
     assert type == :u8
-    assert shape == {2, 3, 3}
-    assert img == <<248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248,
-             248, 248, 248>>
+    assert shape == {2, 3, 4}
+    assert img == <<241, 145, 126, 255, 136, 190, 78, 255, 68, 122, 183, 255, 244, 196, 187, 255,
+             190, 205, 145, 255, 144, 184, 200, 255>>
   end
 
   test "decode jpg from memory" do
@@ -32,7 +32,7 @@ defmodule ImgDecodeTest do
     {:ok, img, shape, type} = ImgDecode.from_memory(buffer)
     assert type == :u8
     assert shape == {2, 3, 3}
-    assert img == <<248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248, 248,
-             248, 248, 248>>
+    assert img == <<180, 128, 70, 148, 128, 78, 89, 134, 101, 222, 170, 112, 182, 162, 112, 112,
+             157, 124>>
   end
 end

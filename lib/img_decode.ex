4e1e64529319c8d8060962c748bf6768ce79dfcb
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2022-01-07 14:14:05 +0000

    init
    Signed-off-by: Cocoa <89497197+cocoa-xu@users.noreply.github.com>

---
+++ lib/img_decode.ex
@@ -0,0 +1,18 @@
+defmodule ImgDecode do
+  @moduledoc """
+  Documentation for `ImgDecode`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ImgDecode.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/img_decode_test.exs
@@ -0,0 +1,8 @@
+defmodule ImgDecodeTest do
+  use ExUnit.Case
+  doctest ImgDecode
+
+  test "greets the world" do
+    assert ImgDecode.hello() == :world
+  end
+end

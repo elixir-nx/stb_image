b3180a96dc595325fa9a41e996c6fd2c49e46d96
Cocoa <i@uwucocoa.moe>
2024-01-24 15:46:11 +0700

    fixed a typo

--- 3rd_party/stb/stb_image.h
+++ 3rd_party/stb/stb_image.h
@@ -2182,7 +2182,7 @@ stbi_inline static int stbi__jpeg_get_bit(stbi__jpeg *j)
 {
    unsigned int k;
    if (j->code_bits < 1) stbi__grow_buffer_unsafe(j);
-   if (j->code_bits < n) return 0; // ran out of bits from stream, return 0s intead of continuing
+   if (j->code_bits < 1) return 0; // ran out of bits from stream, return 0s intead of continuing
    k = j->code_buffer;
    j->code_buffer <<= 1;
    --j->code_bits;

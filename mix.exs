739b09c1d26480653d1d3e1eeafcd1acf62441d0
Cocoa <i@uwucocoa.moe>
2024-03-25 01:47:31 +0800

    update elixir_make to v0.8.3

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule StbImage.MixProject do
     [
       # compilation
       {:cc_precompiler, "~> 0.1.0"},
-      {:elixir_make, "~> 0.8.2"},
+      {:elixir_make, "~> 0.8.3"},
       # optional
       {:nx, "~> 0.4", optional: true},
       {:kino, "~> 0.7", optional: true},
